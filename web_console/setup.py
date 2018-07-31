cef56b73d8ecd8ade59d0f54c08b89db241410c0
ugnelis <ugnius.malukas@gmail.com>
2018-07-31 10:40:21 +0200

    Updated web_console package.

--- web_console/setup.py
+++ web_console/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-# fetch values from package.xml
+# Fetch values from package.xml.
 setup_args = generate_distutils_setup(
     packages=['web_console'],
     package_dir={'': 'src'},
