8fb1e512afb5b09e4e44521d8bd31b7af65cd058
ugnelis <ugnius.malukas@gmail.com>
2018-08-03 13:44:19 +0200

    Fixed Flask app and added front-end (AngularJS) basic app.

---
+++ web_console/src/web_console/controllers.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, Response
+from flask import render_template, url_for, redirect, send_from_directory
+from flask import send_file, make_response, abort
+
+from web_console import app, static_folder
+
+
+@app.route('/')
+def home():
+    return make_response(open(static_folder + '/index.html').read())
