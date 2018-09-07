a0ccd1b3b0656c10ebdeb6d5eea533c4bb81d327
ugnelis <ugnius.malukas@gmail.com>
2018-09-07 15:19:44 +0200

    Fixed mjpeg directive.

--- web_console/src/front_end_app/src/app/directives/mjpeg.directive.js
+++ web_console/src/front_end_app/src/app/directives/mjpeg.directive.js
@@ -14,25 +14,25 @@ function mjpeg() {
 
             scope.$watch('url', function (newVal, oldVal) {
                 if (newVal) {
-                    var iframe = document.createElement('iframe');
+                    let iframe = document.createElement('iframe');
                     iframe.setAttribute('width', scope.width);
                     iframe.setAttribute('frameborder', '0');
                     iframe.setAttribute('scrolling', 'no');
                     element.replaceWith(iframe);
 
-                    var iframeHtml = '<html><head><base target="_parent" /><style type="text/css">html, body { margin: 0; padding: 0; height: 100%; width: 100%; }</style><script> function resizeParent() { var ifs = window.top.document.getElementsByTagName("iframe"); for (var i = 0, len = ifs.length; i < len; i++) { var f = ifs[i]; var fDoc = f.contentDocument || f.contentWindow.document; if (fDoc === document) { f.height = 0; f.height = document.body.scrollHeight; } } }</script></head><body onresize="resizeParent()"><img src="' + newVal + '" style="width: 100%; height: auto" onload="resizeParent()" /></body></html>';
+                    let iframeHtml = '<html><head><base target="_parent" /><style type="text/css">html, body { margin: 0; padding: 0; height: 100%; width: 100%; }</style><script> function resizeParent() { let iframes = window.top.document.getElementsByTagName("iframe"); for (let i = 0, length = iframes.length; i < length; i++) { let iframe = iframes[i]; let iframeDocument = iframe.contentDocument || iframe.contentWindow.document; if (iframeDocument === document) { iframe.height = document.body.scrollHeight; } } }</script></head><body onresize="resizeParent()"><img src="' + newVal + '" style="width: 100%; height: auto" onload="resizeParent()" /></body></html>';
 
-                    var doc = iframe.document;
+                    let iframeDocument = iframe.document;
                     if (iframe.contentDocument) {
-                        doc = iframe.contentDocument;
+                        iframeDocument = iframe.contentDocument;
                     }
                     else if (iframe.contentWindow) {
-                        doc = iframe.contentWindow.document;
+                        iframeDocument = iframe.contentWindow.document;
                     }
 
-                    doc.open();
-                    doc.writeln(iframeHtml);
-                    doc.close();
+                    iframeDocument.open();
+                    iframeDocument.writeln(iframeHtml);
+                    iframeDocument.close();
                 } else {
                     element.html('<span></span>');
                 }
