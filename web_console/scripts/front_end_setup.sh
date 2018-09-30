55efeff3229fd2a87b391b9d16af7daeb7827961
ugnelis <ugnius.malukas@gmail.com>
2018-09-30 12:24:09 +0200

    Updated front_end_setup.sh script.

--- web_console/scripts/front_end_setup.sh
+++ web_console/scripts/front_end_setup.sh
@@ -5,14 +5,14 @@ FORCE=${2:-false}
 FRONT_END_DIR_PATH=../src/front_end_app
 PUBLIC_DIR_PATH=../src/static
 
-# Install node modules if don't exist.
-if [ ! -d "${FRONT_END_DIR_PATH}/node_modules" ]; then
-    npm install ${FRONT_END_DIR_PATH}
-fi
-
 # If folder is empty and it is production mode.
 if [ -z "$(ls ${PUBLIC_DIR_PATH})" ] && [ "${MODE}" == "PRODUCTION" ]; then
     FORCE=true
+fi
+
+# Install node modules if don't exist.
+if [ ! -d "${FRONT_END_DIR_PATH}/node_modules" ]; then
+    npm install --prefix ${FRONT_END_DIR_PATH}
 fi
 
 # Build front-end app.
@@ -22,5 +22,5 @@ fi
 
 # Run front-end app.
 if [ "${MODE}" == "DEVELOPMENT" ]; then
-    npm start
+    npm start --prefix ${FRONT_END_DIR_PATH}
 fi
